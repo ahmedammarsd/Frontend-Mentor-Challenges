0dc59d68ec141cfe292939f7492e0b3cf8d634a6
ahmedammarsd <ahmed091999287@gmail.com>
2024-07-01 17:24:45 +0200

    add link in app.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import RecipePage from "./pages/pecipe_page/RecipePage";
 
 function App() {
   return (
     <>
+      <Link to={"/recipe_page"}>to</Link>
       <Routes>
         <Route path="/recipe_page" element={<RecipePage />} />
       </Routes>
