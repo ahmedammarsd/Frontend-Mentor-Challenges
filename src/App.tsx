715960cabea9882668568ddb8bbb011975e99439
ahmedammarsd <ahmed091999287@gmail.com>
2024-11-28 17:18:22 +0800

    add vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import RecipePage from "./pages/pecipe_page/RecipePage";
-import { useLayoutEffect } from "react";
 
 function App() {
-  const navigate = useNavigate();
-  const pathName = window.location.pathname;
-  useLayoutEffect(() => {
-    if (pathName === "/recipe_page") {
-      navigate("/recipe_page");
-    }
-  }, []);
   return (
     <>
       {/* <Link to={"/recipe_page"}>to</Link> */}
